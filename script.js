0c2e745a2ef428c0f92fe4129c2af3ee6f444869
Mustapha Alaba <alabamustapha@gmail.com>
2021-12-27 15:41:09 +0100

    dropdown fixes

--- script.js
+++ script.js
@@ -30,25 +30,53 @@ const stateObject = {
 
 };
 
-var campaign = document.getElementById("campaign");
+const states = Object.keys(stateObject);
 
-var btn = document.querySelector(".btn");
+const states_el = document.getElementById('state')
+const lga_el = document.getElementById('lga')
+const ward_el = document.getElementById('ward')
 
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  campaign.style.display = "block";
-}
-span.onclick = function() {
-  campaign.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == campaign) {
-    campaign.style.display = "none";
-  }
+
+function populateDropDown(item_array, select_el, default_option='Select'){
+    item_array.sort()
+    select_el.innerHTML = ''
+
+    option_el = document.createElement('option');
+    option_el.textContent = default_option;
+    option_el.value = '';
+    select_el.appendChild(option_el);
+
+    item_array.forEach(item => {
+      option_el = document.createElement('option');
+      option_el.textContent = item;
+      option_el.value = item;
+      select_el.appendChild(option_el);
+    });
+
 }
 
-function states(states){
-    states.foreach(){
+window.onload = function(){
 
-    }
+  populateDropDown(states, states_el, 'State')
+  lga_el.innerHTML = ''
+  ward_el. innerHTML = ''
+
 }
+
+states_el.onchange = function(el){
+
+  ward_el. innerHTML = ''
+  lgas = Object.keys(stateObject[states_el.value])
+  populateDropDown(lgas, lga_el, 'LGA')
+
+}
+
+lga_el.onchange = function(el){
+  
+  wards = stateObject[states_el.value][lga_el.value]
+  console.log(wards)
+  populateDropDown(wards, ward_el, 'Ward')
+
+}
+
+
